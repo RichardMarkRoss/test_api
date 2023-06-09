6010682b6f0edd94db96d6dc0194940ee99bbfeb
richardr <richardr@wsb.co.za>
2023-06-09 10:34:49 +0200

    person, login model

---
+++ test_app/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace test_api.Model
+{
+    public class Person
+    {
+        [Key]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
