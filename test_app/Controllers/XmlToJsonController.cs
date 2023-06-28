3d8319bca66123a1dab1da8edf14ebdaed9ce9ee
richardr <richardr@wsb.co.za>
2023-06-28 11:27:08 +0200

    Lotto Results from Xml to Json

--- test_app/Controllers/XmlToJsonController.cs
+++ test_app/Controllers/XmlToJsonController.cs
@@ -3,6 +3,8 @@ using System.Xml;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -15,8 +17,8 @@ public class XmlToJsonController : ControllerBase
         _httpClientFactory = httpClientFactory;
     }
 
-    [HttpGet]
-    public async Task<IActionResult> Get()
+    [HttpGet("dailylotto")]
+    public async Task<IActionResult> GetDailyLotto()
     {
         var url = "https://www.nationallottery.co.za/xmlfeed/dailylotto.xml";
 
@@ -27,28 +29,35 @@ public class XmlToJsonController : ControllerBase
             {
                 return BadRequest("Unable to retrieve the XML data from the specified URL.");
             }
-
             var xmlContent = await response.Content.ReadAsStringAsync();
-
             try
             {
-                var xmlDoc = new XmlDocument();
-                xmlDoc.LoadXml(xmlContent);
+                var xmlDoc = XDocument.Parse(xmlContent);
+                var json = new JObject();
 
-                var json = JsonConvert.SerializeXmlNode(xmlDoc);
+                foreach (var field in xmlDoc.Descendants("field"))
+                {
+                    var name = field.Attribute("name")?.Value;
+                    var value = field.Attribute("value")?.Value;
 
-                return Ok(json);
+                    if (name != null && value != null)
+                    {
+                        json.Add(name, value);
+                    }
+                }
+
+                return Ok(json.ToString());
             }
-            catch (XmlException)
+            catch (Exception)
             {
                 return BadRequest("The XML data is invalid.");
             }
         }
     }
-    [HttpGet("{type}")]
-    public async Task<IActionResult> Get(string type)
+    [HttpGet("lotto")]
+    public async Task<IActionResult> GetLotto()
     {
-        var url = "https://www.nationallottery.co.za/xmlfeed/"+ type + ".xml";
+        var url = "https://www.nationallottery.co.za/xmlfeed/lotto.xml";
 
         using (var client = _httpClientFactory.CreateClient())
         {
@@ -57,19 +66,212 @@ public class XmlToJsonController : ControllerBase
             {
                 return BadRequest("Unable to retrieve the XML data from the specified URL.");
             }
-
             var xmlContent = await response.Content.ReadAsStringAsync();
-
             try
             {
-                var xmlDoc = new XmlDocument();
-                xmlDoc.LoadXml(xmlContent);
+                var xmlDoc = XDocument.Parse(xmlContent);
+                var json = new JObject();
 
-                var json = JsonConvert.SerializeXmlNode(xmlDoc);
+                foreach (var field in xmlDoc.Descendants("field"))
+                {
+                    var name = field.Attribute("name")?.Value;
+                    var value = field.Attribute("value")?.Value;
 
-                return Ok(json);
+                    if (name != null && value != null)
+                    {
+                        json.Add(name, value);
+                    }
+                }
+
+                return Ok(json.ToString());
             }
-            catch (XmlException)
+            catch (Exception)
+            {
+                return BadRequest("The XML data is invalid.");
+            }
+        }
+    }
+    [HttpGet("lottoplus")]
+    public async Task<IActionResult> GetLottoPlus()
+    {
+        var url = "https://www.nationallottery.co.za/xmlfeed/lottoplus.xml";
+
+        using (var client = _httpClientFactory.CreateClient())
+        {
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest("Unable to retrieve the XML data from the specified URL.");
+            }
+            var xmlContent = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var xmlDoc = XDocument.Parse(xmlContent);
+                var json = new JObject();
+
+                foreach (var field in xmlDoc.Descendants("field"))
+                {
+                    var name = field.Attribute("name")?.Value;
+                    var value = field.Attribute("value")?.Value;
+
+                    if (name != null && value != null)
+                    {
+                        json.Add(name, value);
+                    }
+                }
+
+                return Ok(json.ToString());
+            }
+            catch (Exception)
+            {
+                return BadRequest("The XML data is invalid.");
+            }
+        }
+    }
+    [HttpGet("lottoplus2")]
+    public async Task<IActionResult> GetLottoPlus2()
+    {
+        var url = "https://www.nationallottery.co.za/xmlfeed/lottoplus2.xml";
+
+        using (var client = _httpClientFactory.CreateClient())
+        {
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest("Unable to retrieve the XML data from the specified URL.");
+            }
+            var xmlContent = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var xmlDoc = XDocument.Parse(xmlContent);
+                var json = new JObject();
+
+                foreach (var field in xmlDoc.Descendants("field"))
+                {
+                    var name = field.Attribute("name")?.Value;
+                    var value = field.Attribute("value")?.Value;
+
+                    if (name != null && value != null)
+                    {
+                        json.Add(name, value);
+                    }
+                }
+
+                return Ok(json.ToString());
+            }
+            catch (Exception)
+            {
+                return BadRequest("The XML data is invalid.");
+            }
+        }
+    }
+    [HttpGet("powerball")]
+    public async Task<IActionResult> GetPowerball()
+    {
+        var url = "https://www.nationallottery.co.za/xmlfeed/powerball.xml";
+
+        using (var client = _httpClientFactory.CreateClient())
+        {
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest("Unable to retrieve the XML data from the specified URL.");
+            }
+            var xmlContent = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var xmlDoc = XDocument.Parse(xmlContent);
+                var json = new JObject();
+
+                foreach (var field in xmlDoc.Descendants("field"))
+                {
+                    var name = field.Attribute("name")?.Value;
+                    var value = field.Attribute("value")?.Value;
+
+                    if (name != null && value != null)
+                    {
+                        json.Add(name, value);
+                    }
+                }
+
+                return Ok(json.ToString());
+            }
+            catch (Exception)
+            {
+                return BadRequest("The XML data is invalid.");
+            }
+        }
+    }
+
+    [HttpGet("powerballplus")]
+    public async Task<IActionResult> GetPowerballPlus()
+    {
+        var url = "https://www.nationallottery.co.za/xmlfeed/powerballplus.xml";
+
+        using (var client = _httpClientFactory.CreateClient())
+        {
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest("Unable to retrieve the XML data from the specified URL.");
+            }
+            var xmlContent = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var xmlDoc = XDocument.Parse(xmlContent);
+                var json = new JObject();
+
+                foreach (var field in xmlDoc.Descendants("field"))
+                {
+                    var name = field.Attribute("name")?.Value;
+                    var value = field.Attribute("value")?.Value;
+
+                    if (name != null && value != null)
+                    {
+                        json.Add(name, value);
+                    }
+                }
+
+                return Ok(json.ToString());
+            }
+            catch (Exception)
+            {
+                return BadRequest("The XML data is invalid.");
+            }
+        }
+    }
+
+    [HttpGet("{type}")]
+    public async Task<IActionResult> Get(string type)
+    {
+        var url = "https://www.nationallottery.co.za/xmlfeed/"+ type + ".xml";
+        using (var client = _httpClientFactory.CreateClient())
+        {
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest("Unable to retrieve the XML data from the specified URL.");
+            }
+            var xmlContent = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var xmlDoc = XDocument.Parse(xmlContent);
+                var json = new JObject();
+
+                foreach (var field in xmlDoc.Descendants("field"))
+                {
+                    var name = field.Attribute("name")?.Value;
+                    var value = field.Attribute("value")?.Value;
+
+                    if (name != null && value != null)
+                    {
+                        json.Add(name, value);
+                    }
+                }
+
+                return Ok(json.ToString());
+            }
+            catch (Exception)
             {
                 return BadRequest("The XML data is invalid.");
             }
